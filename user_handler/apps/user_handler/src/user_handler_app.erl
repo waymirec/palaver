d77c7f7a23f17e50c526d4d99ed2dc584e1c0b38
Chris Waymire <waymirec@Chriss-MacBook-Pro-2.local>
2023-06-04 00:53:47 -0700

    Added basic websocket handling

--- user_handler/apps/user_handler/src/user_handler_app.erl
+++ user_handler/apps/user_handler/src/user_handler_app.erl
@@ -10,6 +10,20 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    lager:start(),
+
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", cowboy_static, {priv_file, erws, "index.html"}},
+            {"/websocket", websocket_handler, []}
+        ]}
+    ]),
+
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+
     user_handler_sup:start_link().
 
 stop(_State) ->
---
+++ user_handler/apps/user_handler/src/websocket_handler.erl
@@ -0,0 +1,46 @@
+-module(websocket_handler).
+
+-export([
+  init/2,
+  handle/2,
+  terminate/3]).
+
+-export([
+  websocket_init/1,
+  websocket_handle/2,
+  websocket_info/2,
+  websocket_terminate/3
+]).
+
+
+init(Req, State) ->
+  {cowboy_websocket, Req, State}.
+
+handle(Req, State) ->
+  lager:debug("Request not expected: ~p", [Req]),
+  {ok, Req2} = cowboy_http_req:reply(404, [{'Content-Type', <<"text/html">>}]),
+  {ok, Req2, State}.
+
+websocket_init(State) ->
+  {[{text, <<"Hello!">>}], State}.
+
+
+websocket_handle(Frame = {text, Msg}, State) ->
+  lager:debug("Got Data: ~p", [Msg]),
+  {[{text, << "responding to ", Msg/binary >>}], State};
+
+websocket_handle(_Frame, State) ->
+  {ok, State}.
+
+websocket_info({log, Text}, State) ->
+  lager:debug("Got Info: ~p", [Text]),
+  {[{text, Text}], State};
+
+websocket_info(_Info, State) ->
+  {ok, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
+  ok.
+
+terminate(_Reason, _Req, _State) ->
+  ok.
